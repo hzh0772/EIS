18dc4d265d39cb9fc959af8f4aef1f4fb3a3f874
hzh0772 <hzh0772@users.noreply.github.com>
2017-11-09 22:50:02 +0800

    20171109

--- application/main/controller/Folder.php
+++ application/main/controller/Folder.php
@@ -10,14 +10,45 @@ class Folder extends Controller
 {
     public function index()
     {
+        $this->download(Session::get('userid'));
+        $this->download();
+
         return $this->fetch();
 
     }
 
-    public function download()
+    public function download($userid='')
     {
 
+        if(!''==$userid)
+        {
+            $folder=new FolderModel();
+            $myfilelist = $folder->where('userid',$userid)->select();
+            $this->assign('myfilelist',$myfilelist);
+        }
+        else
+        {
+            $folder1=new FolderModel();
+            $publicfilelist= $folder1->where('public','1')->select();
+            $this->assign('publicfilelist',$publicfilelist);
+        }
 
+    }
+
+    public function delete($fileid)
+    {
+        $folder=new FolderModel();
+        $folder=$folder->where('fileid',$fileid)->find();
+        $path=ROOT_PATH.'public'.DS.'uploads'.DS.$folder->username.DS.$folder->coding;
+        if(!unlink($path))
+        {
+            $this->error('删除失败');
+        }
+        else
+        {
+            $folder->delete();
+            $this->success('删除成功:'.$folder->filename);
+        }
     }
 
     public function upload(Request $request)
@@ -46,12 +77,63 @@ class Folder extends Controller
             if(empty($file)){
                 $this->error('请选择上传文件');
             }
-            $info=$file->validate(['ext'=>'rar,txt,png,jpg,xls,xlsx,ppt,pptx,doc,docx'])->rule('uniqid')->move(ROOT_PATH.'public'.DS.'uploads'.DS.Session::get('username'),$saveName);
+            $info=$file->validate(['ext'=>'rar,txt,png,jpg,xls,xlsx,ppt,pptx,doc,docx'])->rule('uniqid')->move(ROOT_PATH.'public'.DS.'uploads'.DS.Session::get('username'),true,false);
             if($info){
                 $folder=new FolderModel;
                 $folder->filename=$filename;
-                $folder->owner=Session::get('name');
-                $folder->ownerid=Session::get('userid');
+                $folder->coding=$info->getSaveName();
+                $folder->name=Session::get('name');
+                $folder->userid=Session::get('userid');
+                $folder->username=Session::get('username');
+                if($folder->save()){
+                    $this->success('文件上传成功:'.$filename);
+                }
+                else $this->error('数据库提交失败！');
+
+
+
+            }
+            else{
+                $this->error($file->getError());
+            }
+        }
+    }
+
+    public function upload2(Request $request)
+    {
+        $filename=$request->param('publicfile');
+        $rule = [
+
+        ];
+        $msg = [
+
+        ];
+        $data = [
+            'month'  =>$filename,
+        ];
+        $validate = new Validate($rule,$msg);
+        $result   = $validate->check($data);
+        if(!$result){
+            $this->error($validate->getError());
+        }
+        else{
+            $file=$request->file('publicfile');
+            $filename= $file->getInfo('name');
+            $saveName = iconv("UTF-8", "GB2312", $filename);
+
+
+            if(empty($file)){
+                $this->error('请选择上传文件');
+            }
+            $info=$file->validate(['ext'=>'rar,txt,png,jpg,xls,xlsx,ppt,pptx,doc,docx'])->rule('uniqid')->move(ROOT_PATH.'public'.DS.'uploads'.DS.Session::get('username'),true,false);
+            if($info){
+                $folder=new FolderModel;
+                $folder->filename=$filename;
+                $folder->coding=$info->getSaveName();
+                $folder->name=Session::get('name');
+                $folder->userid=Session::get('userid');
+                $folder->username=Session::get('username');
+                $folder->public=1;
                 if($folder->save()){
                     $this->success('文件上传成功:'.$filename);
                 }
